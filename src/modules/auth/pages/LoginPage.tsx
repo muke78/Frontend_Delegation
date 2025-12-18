fd99a3a97890ccf25067a5725bdc24f9ea4986a0
Khelde <94265348+muke78@users.noreply.github.com>
2025-12-18 02:36:14 -0600

    Update LoginPage.tsx

--- src/modules/auth/pages/LoginPage.tsx
+++ src/modules/auth/pages/LoginPage.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from '@/components/ui/label';
-import { Icons } from '@/styles/Icons.tsx';
+import { Icons } from '@/styles/Icons.ts';
 import {
   Field,
   FieldDescription,
